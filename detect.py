01b349ec4f10d0fe9cdce2e498b6c36de9bc0104
Mark Sprague <spraguem@ecu.edu>
2020-02-04 14:57:12 -0500

    Added muon_detect, a command-line interface to detect; detect.py ready to test.

--- detect.py
+++ detect.py
@@ -1,18 +1,27 @@
 from __future__ import division, print_function
 
-def detect(port, outfile='muondata.txt', fmode='w')
+def detect(port, outfile='muondata.txt', fmode='w', sampletime=0, ndecays=0, 
+    killswitch=None):
     import numpy as np
     import re
     import serial
     import time
+    if ndecays == 0:
+        ndecays = 1000000 # Default maximum number of decays
+    if sampletime == 0:
+        sampletime = 14 * 24 * 3600 # Default maximum sample time 2 weeks
     reading = True
+    decay_count = 0
+    muon_count = 0
     with open(outfile, fmode) as output:
         detector = serial.Serial(port, baudrate=115200, timeout=1)
+        t0 = time.time()
         while reading:
             rawdata=detector.read(1024)
-            tstamp = 
+            tstamp = time.time()
             data0x = np.frombuffer(re.sub(b'\r\n*', b'', rawdata), dtype='|S3')
             data_ns = np.array([20 * int(n, 16) for n in data0x])
+            muon_count += data_ns.size
             timeouts = np.flip(np.argwhere(data_ns==20000).flatten())
             prev = timeouts[0]
             count = 1
@@ -38,4 +47,11 @@ def detect(port, outfile='muondata.txt', fmode='w')
             data_out = (np.concatenate(([data_ns], 
                 [tstamp * np.ones(data_ns.size, dtype=np.int)]), axis=0).T)
             np.savetxt(output, data_out, fmt='%d')
-
+            decay_count += (data_ns < 20000).size
+            etime = tstamp - t0
+            if decay_count >= ndecays or etime >= sampletime:
+                reading = False
+            if killswitch:
+                if killswitch.value == False:
+                    reading = False
+    return muon_count, decay_count, etime
