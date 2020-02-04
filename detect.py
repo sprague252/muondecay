a8d78a39f13ed618e7a9b3854627812049214c2c
Mark Sprague <spraguem@ecu.edu>
2020-02-03 23:27:19 -0500

    Did not commit the changes to detect.py. Here they are!

--- detect.py
+++ detect.py
@@ -1,31 +1,41 @@
 from __future__ import division, print_function
 
-def detect(port, outfile='muondata.txt', appnd=False)
+def detect(port, outfile='muondata.txt', fmode='w')
     import numpy as np
     import re
     import serial
     import time
     reading = True
-    decay_time = np.array([], dtype=np.int)
-    decay_stamp = np.array([], dtype=np.int)
-    if appnd=True:
-        fmode = 'a'
-    else:
-        fmode = 'w'
-    with open(outfile,fmode) as output:
+    with open(outfile, fmode) as output:
         detector = serial.Serial(port, baudrate=115200, timeout=1)
         while reading:
             rawdata=detector.read(1024)
+            tstamp = 
             data0x = np.frombuffer(re.sub(b'\r\n*', b'', rawdata), dtype='|S3')
             data_ns = np.array([20 * int(n, 16) for n in data0x])
-            
+            timeouts = np.flip(np.argwhere(data_ns==20000).flatten())
+            prev = timeouts[0]
+            count = 1
+            indlist = np.array([], dtype=np.int)
+            countlist = np.array([], dtype=np.int)
+            for val in timeouts[1:]:
+                if val == prev - 1:
+                    count += 1
+                else:
+                    indlist = np.insert(indlist, 0, prev)
+                    if count == 1:
+                        countlist = np.insert(countlist, 0, 40000)
+                    else:
+                        countlist = np.insert(countlist, 0, 40000+count)
+                    count = 1
+                prev = val
+            indlist = np.insert(indlist, 0, prev)
+            if count == 1:
+                countlist = np.insert(countlist, 0, 40000)
+            else:
+                countlist = np.insert(countlist, 0, 40000+count)            
+            data_ns[indlist] = countlist
+            data_out = (np.concatenate(([data_ns], 
+                [tstamp * np.ones(data_ns.size, dtype=np.int)]), axis=0).T)
+            np.savetxt(output, data_out, fmt='%d')
 
-
-def island_cumsum_vectorized(a):
-    """Vectorized function to count consecutive zeros in a numpy array. Taken from 
-    https://stackoverflow.com/questions/42129021/counting-consecutive-1s-in-numpy-array
-    """
-    a_ext = np.concatenate(( [0], a, [0] ))
-    idx = np.flatnonzero(a_ext[1:] != a_ext[:-1])
-    a_ext[1:][idx[1::2]] = idx[::2] - idx[1::2]
-    return a_ext.cumsum()[1:-1]
