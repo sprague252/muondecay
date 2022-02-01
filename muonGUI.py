35101d01cc35f976ddc021e60e148a9b79294d47
Mark Sprague <spraguem@ecu.edu>
2022-01-31 23:53:50 -0500

    Fixed foreground color and quit dialog in muonGUI.

--- muonGUI.py
+++ muonGUI.py
@@ -18,11 +18,14 @@ def muon_GUI():
 
     class Param:
 
-        def __init__(self, master, label, val, row=None, show=True):
+        def __init__(self, master, label, val, foreground="black",
+            row=None, show=True):
             self.name = label
-            self.label = tk.Label(master, text=label+':')
+            self.label = tk.Label(master, text=label+':',
+                foreground=foreground)
             vtext = '{:g}'.format(val)
-            self.value = tk.Label(master, text=vtext)
+            self.value = tk.Label(master, text=vtext,
+                foreground=foreground)
             if show:
                 if row == None:
                     self.label.grid(column=0, sticky=tk.E)
@@ -62,26 +65,30 @@ def muon_GUI():
         except:
             config_win = tk.Toplevel()
             config_win.title("Configuration")
-            port_frame = tk.LabelFrame(config_win, text="Select Port")
+            port_frame = tk.LabelFrame(config_win, text="Select " +
+                "Port", foreground="black")
             port_frame.pack()
             for dv in devs:
                 dv_rbutton = tk.Radiobutton(port_frame, text=dv, 
-                    variable=device, value=dv)
+                    foreground="black", variable=device, value=dv)
                 dv_rbutton.pack(anchor=tk.W)
-            outfile_frame = tk.LabelFrame(config_win, text="Output File")
+            outfile_frame = tk.LabelFrame(config_win, text="Output" +
+                " File", foreground="black")
             outfile_frame.pack()
-            outfile_button = tk.Button(outfile_frame, text="Select File",
-                command = outfile_dialog)
+            outfile_button = tk.Button(outfile_frame, text="Select" +
+                " File", foreground="black", command = outfile_dialog)
             outfile_button.grid(row=0, column=0)
-            outfile_label = tk.Label(outfile_frame, textvariable=outfname)
+            outfile_label = tk.Label(outfile_frame,
+                textvariable=outfname, foreground="black")
             outfile_label.grid(row=0, column=1)
             appnd = tk.BooleanVar()
             appnd.set(False)
             appnd_check = tk.Checkbutton(outfile_frame, 
-                text='Append to File', variable=appnd)
+                text="Append to File", variable=appnd,
+                foreground="black")
             appnd_check.grid(row=1, column=1, sticky=tk.E)
-            ok_button = tk.Button(config_win, text="OK", 
-                command=config_win.destroy)
+            ok_button = tk.Button(config_win, text="OK",
+                foreground="black", command=config_win.destroy)
             ok_button.pack()       
 
     def outfile_dialog():
@@ -98,7 +105,11 @@ def muon_GUI():
         if fname:
             outfname.set(fname)
         
-        
+    def quit_dialog():
+        from tkinter import messagebox
+        answer = messagebox.askyesno("Question","Do you want to quit?")
+        if answer==True:
+           master.destroy()     
   
 
     master = tk.Tk()
@@ -116,29 +127,34 @@ def muon_GUI():
     device.set("/dev/null")
 
 
-    control_frame = tk.LabelFrame(left_frame, text="Control", padx=75)
+    control_frame = tk.LabelFrame(left_frame, text="Control",
+        foreground="black", padx=75)
     control_frame.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N)
     config_button = tk.Button(control_frame,  text="Configure",
-        width=10)
+        width=10, foreground="black")
     config_button['command'] = open_window
     config_button.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
     config_button.columnconfigure(0, weight=1)
     config_button.rowconfigure(0, weight=1)
-    start_button = tk.Button(control_frame,  text="Start", width=10)
+    start_button = tk.Button(control_frame, text="Start", width=10,
+        foreground="black")
     start_button.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N)
-    pause_button = tk.Button(control_frame,  text="Pause", width=10)
+    pause_button = tk.Button(control_frame, text="Pause", width=10,
+        foreground="black")
     pause_button.grid(row=2, column=0, sticky=tk.W+tk.E+tk.N)
-    fit_button = tk.Button(control_frame,  text="Fit", width=10)
+    fit_button = tk.Button(control_frame, text="Fit", width=10,
+        foreground="black")
     fit_button.grid(row=3, column=0, sticky=tk.W+tk.E+tk.N)
-    rawdata_button = tk.Button(control_frame,  text="View Raw Data",
-        width=10)
+    rawdata_button = tk.Button(control_frame, text="View Raw Data",
+        width=10, foreground="black")
     rawdata_button.grid(row=4, column=0, sticky=tk.W+tk.E+tk.N)
-    quit_button = tk.Button(control_frame,  text="Quit", width=10, 
-        foreground="red", command=master.destroy)
+    quit_button = tk.Button(control_frame, text="Quit", width=10, 
+        foreground="red")
+    quit_button['command'] = quit_dialog
     quit_button.grid(row=5, column=0, sticky=tk.W+tk.E+tk.N)
 
-    monitor_frame = tk.LabelFrame(left_frame, text="Monitor", padx=75,
-        pady=5)
+    monitor_frame = tk.LabelFrame(left_frame, text="Monitor",
+        foreground="black", padx=75, pady=5)
     monitor_frame.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N)
     tt=0
     nmuon=0
@@ -153,8 +169,8 @@ def muon_GUI():
     decayrate_Param = Param(monitor_frame, 'Decay Rate (1/min)',
         decayrate, row=4)
 
-    rate_frame  = tk.LabelFrame(left_frame, text="Rate Meter", padx=5,
-        pady=5)
+    rate_frame  = tk.LabelFrame(left_frame, text="Rate Meter",
+        foreground="black", padx=5, pady=5)
     rate_frame.grid(row=2, column=0, sticky=tk.W+tk.E+tk.N)
 
     ratefig = Figure(figsize=(3, 2), dpi=100)
@@ -166,12 +182,13 @@ def muon_GUI():
 
 
     histogram_frame = tk.LabelFrame(right_frame, text="Muon Decay " +
-        "Time Histogram", padx=5, pady=5)
+        "Time Histogram", foreground="black", padx=5, pady=5)
     histogram_frame.grid(row=0, column=0, sticky=tk.NW)
     linlog_button = tk.Button(histogram_frame,  text="Change " +
-        "y-scale Linear/Log", width=20)
+        "y-scale Linear/Log", foreground="black", width=20)
     linlog_button.grid(row=0, column=0, sticky=tk.W)
-    export_button = tk.Button(histogram_frame,  text="Export Histogram",
+    export_button = tk.Button(histogram_frame,  text="Export " +
+        "Histogram", foreground="black",
         width=20)
     export_button.grid(row=0, column=1, sticky=tk.E)
 
@@ -183,8 +200,9 @@ def muon_GUI():
     canvas.get_tk_widget().grid(row=1, column=0, columnspan=2,
         sticky=tk.W+tk.E+tk.N)
 
-    detector_frame = tk.LabelFrame(right_frame, text="Muons " + 
-        "Through Detector", width=400, height=100, padx=5, pady=5)
+    detector_frame = tk.LabelFrame(right_frame, text="Muons " +
+        "Through Detector", foreground="black", width=400,
+        height=100, padx=5, pady=5)
     detector_frame.grid(row=1, column=0, sticky=tk.W+tk.E+tk.N)
 
     master.mainloop()
